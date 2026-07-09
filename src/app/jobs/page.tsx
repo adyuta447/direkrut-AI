30e33f43b362685e97864e64910e947c98cc219e
adyuta447 <yutaagra@gmail.com>
2026-07-10 02:09:11 +0700

    fix: show job detail as mobile bottom sheet instead of below the list

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -10,7 +10,9 @@ import { FooterRevealBody } from "../../components/atoms/shared/FooterRevealBody
 import { JobSearchHeader } from "../../components/organisms/jobs/JobSearchHeader";
 import { JobList } from "../../components/organisms/jobs/JobList";
 import { JobDetailPanel } from "../../components/organisms/jobs/JobDetailPanel";
+import { JobDetailSheet } from "../../components/organisms/jobs/JobDetailSheet";
 import { JobsEmptyState } from "../../components/molecules/jobs/JobsEmptyState";
+import { CtaBanner } from "@/components/organisms/landing/CtaBanner";
 
 export default function JobsPage() {
   return (
@@ -31,29 +33,41 @@ function JobsPageContent() {
   return (
     <div className="min-h-screen bg-canvas text-ink font-sans overflow-x-clip">
       <FooterRevealBody className="min-h-screen flex flex-col">
-      <SiteHeader />
+        <SiteHeader />
 
-      <JobSearchHeader filters={filters} resultCount={filters.filteredJobs.length} />
+        <JobSearchHeader
+          filters={filters}
+          resultCount={filters.filteredJobs.length}
+        />
 
-      <div id="job-results" className="flex-1 max-w-[1584px] mx-auto w-full px-6 lg:px-10 py-8 scroll-mt-20">
-        {filters.filteredJobs.length === 0 ? (
-          <JobsEmptyState onClearFilters={filters.clearFilters} />
-        ) : (
-          <div className="flex flex-col lg:flex-row gap-6 items-start">
-            <JobList
-              jobs={filters.paginatedJobs}
-              selectedJob={filters.selectedJob}
-              activeJobId={filters.activeJob?.id}
-              onSelect={filters.setSelectedJob}
-              page={filters.page}
-              pageCount={filters.pageCount}
-              onPageChange={filters.goToPage}
-            />
-            {filters.activeJob && <JobDetailPanel job={filters.activeJob} />}
-          </div>
-        )}
-      </div>
+        <div
+          id="job-results"
+          className="flex-1 max-w-[1584px] mx-auto w-full px-6 lg:px-10 py-8 scroll-mt-20"
+        >
+          {filters.filteredJobs.length === 0 ? (
+            <JobsEmptyState onClearFilters={filters.clearFilters} />
+          ) : (
+            <div className="flex flex-col lg:flex-row gap-6 items-start">
+              <JobList
+                jobs={filters.paginatedJobs}
+                selectedJob={filters.selectedJob}
+                activeJobId={filters.activeJob?.id}
+                onSelect={filters.setSelectedJob}
+                page={filters.page}
+                pageCount={filters.pageCount}
+                onPageChange={filters.goToPage}
+              />
+              {filters.activeJob && <JobDetailPanel job={filters.activeJob} />}
+            </div>
+          )}
+        </div>
+        <CtaBanner />
       </FooterRevealBody>
+
+      <JobDetailSheet
+        job={filters.selectedJob ? filters.activeJob : null}
+        onClose={() => filters.setSelectedJob(null)}
+      />
 
       <SiteFooter />
     </div>
